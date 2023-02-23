ae5b0ca6fb5878d6998ea075b8d4042c8df41004
Babatunde Owolabi <chatwittundeowolabi@gmail.com>
2023-02-23 15:28:20 +0100

    arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -1,29 +1,37 @@
-// const character = "luigi";
-// console.log(character);
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-// const inputs = document.querySelectorAll("input");
-// console.log(inputs);
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
 
-// inputs.forEach(input => {
-//   console.log(input);
-// });
+let numbers = [10, 20, 12, 15];
 
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
 
-// character = 20;
-character = 'luigi';
+let mixed = ['ken', 4, 'chun-li', 8, 9];
 
-// age = 'yoshi';
-age = 40;
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
 };
 
-// console.log(area('hello'));
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: ['running'],
+};
